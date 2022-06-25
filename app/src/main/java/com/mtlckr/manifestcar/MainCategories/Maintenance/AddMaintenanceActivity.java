1da92308a155092cfdbd4641d7c952e371698cf8
Mutlu Çakır <mutlu_cakir@hotmail.com>
2022-06-25 16:59:05 +0300

    <3

---
+++ app/src/main/java/com/mtlckr/manifestcar/MainCategories/Maintenance/AddMaintenanceActivity.java
@@ -0,0 +1,16 @@
+package com.mtlckr.manifestcar.MainCategories.Maintenance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.mtlckr.manifestcar.R;
+
+
+public class AddMaintenanceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_maintenance);
+    }
+}
